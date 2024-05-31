6ae2f2064a27266320f080284f329f1a4fa20531
Muhammed Javad <muhammedjavad@Muhammeds-MacBook-Air.local>
2024-05-31 20:30:11 +0530

    User profile consuming

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const dotenv = require("dotenv");
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+
 const profileDatabseConnection = require("./config/db/connection");
 const userRouter = require("./router/userRouter/index")
 const adminRouter = require("./router/adminRouter/index")
